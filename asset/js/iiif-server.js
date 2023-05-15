14d145a5ab24724d8000fec6c8cf3b047f09ca9c
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-05-15 00:00:00 +0000

    Added a message when copy failed.

--- asset/js/iiif-server.js
+++ asset/js/iiif-server.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
 
     $('.iiif-copy').on('click', function(e) {
         const button = $(this);
-        navigator.clipboard.writeText(button.data('iiif-url'));
-        alert(button.data('textCopied') ? button.data('textCopied') : 'Url of the IIIF manifest copied in clipboard!');
+        try {
+            navigator.clipboard.writeText(button.data('iiif-url'));
+            alert(button.data('textCopied') ? button.data('textCopied') : 'Url of the IIIF manifest copied in clipboard!');
+        } catch (err) {
+            alert(button.data('textFailed') ? button.data('textFailed') : 'Unable to copy url in clipboard!');
+        }
     });
 
 });
