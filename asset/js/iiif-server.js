2a037dbdb30049f6a29bfba93f07b66ad604685d
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-07-01 00:00:00 +0000

    Made button iiif-copy compatible with unsecure connection.

--- asset/js/iiif-server.js
+++ asset/js/iiif-server.js
@@ -2,11 +2,19 @@ $(document).ready(function () {
 
     $('.iiif-copy').on('click', function(e) {
         const button = $(this);
-        try {
-            navigator.clipboard.writeText(button.data('iiif-url'));
-            alert(button.data('textCopied') ? button.data('textCopied') : 'Url of the IIIF manifest copied in clipboard!');
-        } catch (err) {
-            alert(button.data('textFailed') ? button.data('textFailed') : 'Unable to copy url in clipboard!');
+        const iiifUrl = button.data('iiif-url');
+        // Navigator clipboard requires a secure connection (https).
+        if (navigator.clipboard) {
+            navigator.clipboard
+                .writeText(iiifUrl)
+                .then(() => {
+                    alert(button.data('textCopied') ? button.data('textCopied') : 'Url of the IIIF manifest copied in clipboard!');
+                })
+                .catch(() => {
+                    alert(button.data('textFailed') ? button.data('textFailed') : 'Unable to copy url in clipboard!');
+                });
+        } else {
+            alert(iiifUrl);
         }
     });
 
