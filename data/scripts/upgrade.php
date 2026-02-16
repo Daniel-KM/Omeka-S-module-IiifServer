e6d2dfd110d4b2afb13b8dd26397715438e664f6
Daniel Berthereau <Daniel.git@Berthereau.net>
2026-02-16 00:00:00 +0000

    Updated Common version.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -30,10 +30,10 @@ $entityManager = $services->get('Omeka\EntityManager');
 $defaultConfig = require dirname(__DIR__, 2) . '/config/module.config.php';
 $defaultSettings = $defaultConfig['iiifserver']['config'];
 
-if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.69')) {
+if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.79')) {
     $message = new \Omeka\Stdlib\Message(
         $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
-        'Common', '3.4.69'
+        'Common', '3.4.79'
     );
     throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message);
 }
